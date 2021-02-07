1d4fac4e43f1e66326b8970c6470722278a99680
Bas Pennings <baspster@gmail.com>
2021-02-07 11:21:06 +0100

    small unit test

--- logic.py
+++ logic.py
@@ -1,3 +1,7 @@
+import unittest
+
+# From http://openbookproject.net/courses/python4fun/logic.html
+
 class Connector:
     def __init__(self, owner, name, activates = 0, monitor = 0):
         self.value = None
@@ -16,7 +20,7 @@ class Connector:
     def set(self, value):
         if self.value == value:
             return
-        self.value = value
+        self.value = int(value)
         if self.activates:
             self.owner.eval()
         if self.monitor:
@@ -37,3 +41,26 @@ class Gate2(LC):
         self.a = Connector(self, 'a', activates = 1)
         self.b = Connector(self, 'b', activates = 1)
         self.out = Connector(self, 'out')
+
+class Nand(Gate2):
+    def __init__(self, name):
+        Gate2.__init__(self, name)
+    
+    def eval(self):
+        self.out.set(not (self.a.value and self.b.value))
+
+class Tests(unittest.TestCase):
+    def test_nand(self):
+        g = Nand('NAND')
+        self.assertIsNone(g.out.value)
+        g.a.set(0)
+        self.assertEqual(g.out.value, 1)
+        g.b.set(0)
+        self.assertEqual(g.out.value, 1)
+        g.a.set(1)
+        self.assertEqual(g.out.value, 1)
+        g.b.set(1)
+        self.assertEqual(g.out.value, 0)
+
+if __name__ == '__main__':
+    unittest.main()
